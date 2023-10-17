19f05414ea7377ada12959e3b6eec3b8bd167d1d
yuxinliuhubert <52901645+yuxinliuhubert@users.noreply.github.com>
2023-10-17 10:35:38 -0700

    Update particle_path_finder.py

--- Phase4/Variable_Speeds_Python/particle_path_finder.py
+++ Phase4/Variable_Speeds_Python/particle_path_finder.py
@@ -1,21 +1,33 @@
 import numpy as np
-from T import T
-def track_particles(data_set, alpha):
-    M, N = data_set.shape[0], data_set.shape[1] // 2  # Assuming each particle has 2 coordinates (x, z)
-    tracked_particles = np.zeros((M, 2 * N))
-    tracked_particles[0] = data_set[0]
+from T import T  # Assuming this function rotates a particle by an angle 'alpha'
 
-    for m in range(1, M):
-        for n in range(0, 2 * N, 2):
-            particle = data_set[m, n:n+2]
-            # rotated_particle = T(particle, alpha)
-            previous_shot = tracked_particles[m-1].reshape(N, 2)
-            closest_particle_index = find_closest_particle(particle, previous_shot)
-            tracked_particles[m, 2*closest_particle_index:2*closest_particle_index+2] = particle
+class ParticlePathFinder:
 
-    return tracked_particles
+    def __init__(self, currentParticleNum=0, previousParticleNum=0) -> None:
+        self.currentParticleNum = currentParticleNum
+        self.previousParticleNum = previousParticleNum
+        self.previousShot = np.zeros((self.previousParticleNum, 1))
+        self.newShot = np.zeros((self.currentParticleNum, 1))
 
-def find_closest_particle(particle, shot):
-    distances = np.linalg.norm(shot - particle, axis=1)
-    return np.argmin(distances)
+    def track_particles(self, data_set, alpha):
+        
+        M, N = data_set.shape[0], data_set.shape[1] // 2  # Assuming each particle has 2 coordinates (x, z)
+        tracked_particles = np.zeros((M, 2 * N))
+        tracked_particles[0] = data_set[0]
 
+        for m in range(1, M):
+            for n in range(0, 2 * N, 2):
+                particle = data_set[m, n:n+2]
+                previous_shot = tracked_particles[m-1].reshape(N, 2)
+                closest_particle_index = self.find_closest_particle(particle, previous_shot)
+                tracked_particles[m, 2*closest_particle_index:2*closest_particle_index+2] = particle
+
+        return tracked_particles
+
+    def find_closest_particle(self, particle, shot):
+        distances = np.linalg.norm(shot - particle, axis=1)
+        return np.argmin(distances)
+
+    # Additional methods can be added as needed. For instance:
+    # def outOfFOVCheck(self, M):
+    #     utils.raiseNotDefined()
