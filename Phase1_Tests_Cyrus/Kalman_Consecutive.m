65872016d888e6b52097b3fc13d4580a91cfffc1
yuxinliuhubert <52901645+yuxinliuhubert@users.noreply.github.com>
2023-10-23 23:28:26 -0700

    Organized files

--- Phase1_Tests_Cyrus/Kalman_Consecutive.m
+++
@@ -1,4 +0,0 @@
-function [percent_error]=Kalman_Consecutive(true_position_3d,noise,NOS, theta_degree)
-delta_T=1; SRD=1;RDD=1;
-[kalman_position, avg_position] = Phase1_pt_3d(true_position_3d, noise,delta_T, NOS, theta_degree, SRD, RDD);
-percent_error=abs((avg_position(1)-true_position_3d(1))/true_position_3d(1))*100;
